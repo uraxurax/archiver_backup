361e19edad8a3144f288ae95e7c32884b12f6ec4
Hiromitsu Matsuura <hiro@Hiromitsu-no-MacBook-Pro.local>
2015-03-11 02:30:18 +0900

    update sample code

--- samples.rb
+++ samples.rb
@@ -1,35 +1,82 @@
+
+require 'date'
 require './homes_archiver'
+require './region_controller_module'
+require './archiver_module'
 
+include RegionControllerModule
+include ArchiverModule
 
-opts = {
-  :proxy_host => 'localhost',
-  :proxy_port => '5432',
-}
-#url = "http://www.homes.co.jp/tochi/saitama/warabi-city/list/"
+def capture_stdout
+  out = StringIO.new
+  $stdout = out
+  yield
+  return out.string
+ensure
+  $stdout = STDOUT
+end
 
-#page_urls = get_page_urls(url, opts);
-#page_urls.each{|url|
-#  puts url;
+def capture_stderr
+  out = StringIO.new
+  $stderr = out
+  yield
+  return out.string
+ensure
+  $stderr = STDERR
+end
+
+#opts = {
+#  :proxy_host => 'localhost',
+#  :proxy_port => '5432',
 #}
 
-#prop_urls = get_prop_urls(page_urls, opts)
-#prop_urls.each{|url|
-#  puts url;
-#}
+opts = {}
 
+#pref_list = [:saitama]
+pref_list = [:tokyo]
 
 homes_archiver = HomesArchiver.new(opts)
 
-#prop_urls = homes_archiver.get_prop_urls_from_regions([:warabi])
-prop_urls = homes_archiver.get_prop_urls_from_regions([:saitama_nishiku])
-#prop_urls = homes_archiver.get_prop_urls_from_regions([:saitama])
-puts prop_urls.length
-prop_urls.each {|prop_url|
-  p prop_url
-}
+date = Date::today
 
-#PR
-#//*[@id="kksframelist"]/div[1]/div[1]/div[1]/h3/a
+stdout_file = "stdout_" + date.to_s + ".txt"
+stderr_file = "stderr_" + date.to_s + ".txt"
 
-#normal
-#//*[@id="prg-mod-bukkenList"]/div[2]/div[2]/div[1]/div[2]/div[1]/h3/a
+cstderr = capture_stderr do
+  pref_list.each{|pref|
+    pref_prop_file = pref.to_s + "_prop_list_" + date.to_s + ".txt"
+    pref_arch_file = pref.to_s + "_arch_list_" + date.to_s + ".txt"
+    
+    city_list = get_city_list(pref)
+    city_list.each{|city|
+      puts city
+      arch_urls = []
+      
+      prop_urls = homes_archiver.get_prop_urls_from_city(city)
+      prop_urls.each{|prop_url|
+        archived_url = try_archive(prop_url)
+        unless archived_url == nil
+          arch_urls.push(archived_url) 
+          sleep(1)
+        end
+      }
+      puts "prop_urls.length() = " + prop_urls.length().to_s
+      File.open(pref_prop_file, "a") do |file|
+        file.write(city.to_s + ":" + Time.now().to_s + "\n")
+        prop_urls.each{ |prop_url|
+          file.write(prop_url + "\n")
+        }
+      end
+      puts "arch_urls.length() = " + arch_urls.length().to_s
+      File.open(pref_arch_file, "a") do |file|
+        file.write(city.to_s + ":" + Time.now().to_s + "\n")
+        arch_urls.each{ |arch_url|
+          file.write(arch_url + "\n")
+        }
+      end
+    }
+  }
+end
+File.open(stderr_file, "a") do |file|
+  file.write(cstderr)
+end
